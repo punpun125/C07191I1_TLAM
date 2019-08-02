c9e507aa938df7a8e29e0868b23016eb4a234210
punpun <mushroomokayama.jp@gmail.com>
2019-08-02 15:58:50 +0700

    fix money converter

--- Session2.2/2.2.1.js
+++
@@ -1,7 +0,0 @@
-function moneyout(){
-    let F = parseInt(document.getElementById("X").value) * parseInt(document.getElementById("A").value) / parseInt(document.getElementById("B").value);
-    let D = F.toFixed(2);
-    let bluecolor= "Result "+D;
-    let outputmsg= bluecolor.fontcolor("blue");
-    document.getElementById("Outputmoney").innerHTML = outputmsg;
-}
